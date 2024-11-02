accd21f7555063b7d407fba4e9e37a8d420eac89
Jan Sobolewski <59810745+Jassobolewski@users.noreply.github.com>
2024-11-02 14:13:56 -0400

    Update PlayerMovement.cs
    Movement wokrs

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,16 +3,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public PlayerMovement =0.0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
 
+    public Rigidbody2D rb; 
+    Vector2 movement;
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
