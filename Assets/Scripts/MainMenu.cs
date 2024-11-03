68218032603dc2924b6d63fc9eb54daa92dc1850
Last4 <110793055+Last4@users.noreply.github.com>
2024-11-03 11:23:11 -0500

    Main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OnOptionsButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+}
